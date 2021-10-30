933ee6b4057ebd9203b6ee988b62725ad151c0d8
code <code@ub4>
2021-10-30 16:28:41 +0200

    updates from make format

--- manual.py
+++ manual.py
@@ -2,13 +2,13 @@
 
 import sys
 
-def add(x,y):
-    return x+y
 
+def add(x, y):
+    return x + y
 
 
 if __name__ == "__main__":
     print(sys.argv)
     one = int(sys.argv[1])
     two = int(sys.argv[2])
-    print(add(one,two))
+    print(add(one, two))
